f750b538afcd9b0abdb0f7a39f65c3100164e9ad
felipejbs <felipejeronimobs@gmail.com>
2024-12-14 20:22:08 -0300

    adicionando PK

--- main.py
+++ main.py
@@ -11,17 +11,31 @@ Base = declarative_base()
 class Usuario(Base):
     __tablename__ = "usuarios"
 
-    id = Column("id", Integer)
+    id = Column("id", Integer, primary_key = True, autoincrement=True)
     nome = Column("nome", String)
     email = Column("email", String)
     senha = Column("senha", String)
     ativo = Column("ativo", Boolean)
 
+    def __init__(self, nome, email, senha, ativo=True):
+        self.nome
+        self.email
+        self.senha
+        self.ativo
+
+
 #Livros
 class Livro(Base):
     __tablename__ = "livros"
 
-    id = Column("id", Integer)
+    id = Column("id", Integer, primary_key = True, autoincrement=True)
     titulo = Column("titulo", String)
     qtde_paginas = Column("qtde_paginas", Integer)
     dono = Column("dono", ForeignKey("usuarios.id"))
+
+    def __init__(self, titulo, qtd_paginas, dono):
+        self.titulo
+        self.qtde_paginas
+        self.dono
+
+Base.metadata_create_all(bind=db)
