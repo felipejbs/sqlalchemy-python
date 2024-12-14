dc636f30e70027e0a0bad8b46049649ce0d3efd6
felipejbs <felipejeronimobs@gmail.com>
2024-12-14 19:58:02 -0300

    adicionando FK e tablename

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sqlalchemy import creat_engine, Column, String, Integer, Boolean
+from sqlalchemy import ForeignKey, creat_engine, Column, String, Integer, Boolean
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 db = creat_engine("sqlite:///meubanco.db")
@@ -9,6 +9,8 @@ Base = declarative_base()
 
 # Tabelas
 class Usuario(Base):
+    __tablename__ = "usuarios"
+
     id = Column("id", Integer)
     nome = Column("nome", String)
     email = Column("email", String)
@@ -17,7 +19,9 @@ class Usuario(Base):
 
 #Livros
 class Livro(Base):
+    __tablename__ = "livros"
+
     id = Column("id", Integer)
     titulo = Column("titulo", String)
     qtde_paginas = Column("qtde_paginas", Integer)
-    dono = Column("dono", String)
+    dono = Column("dono", ForeignKey("usuarios.id"))
